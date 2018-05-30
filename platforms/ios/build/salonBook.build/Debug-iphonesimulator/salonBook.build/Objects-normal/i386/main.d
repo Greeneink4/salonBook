e4546bc78e373d8f1db546ebab0a932db1269343
Michael Greene <greeneink4@gmail.com>
2018-05-30 17:54:17 -0600

    tns update next changes

---
+++ platforms/ios/build/salonBook.build/Debug-iphonesimulator/salonBook.build/Objects-normal/i386/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/michaelgreene/projects/salonBook/platforms/ios/salonBook/salonBook-Prefix.pch \
+  /Users/michaelgreene/projects/salonBook/platforms/ios/internal/main.m \
+  /Users/michaelgreene/projects/salonBook/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/michaelgreene/projects/salonBook/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/michaelgreene/projects/salonBook/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/michaelgreene/projects/salonBook/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/michaelgreene/projects/salonBook/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/michaelgreene/projects/salonBook/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/michaelgreene/projects/salonBook/platforms/ios/internal/TNSDebugging.h \
+  /Users/michaelgreene/projects/salonBook/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
